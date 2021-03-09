b5764664c33eee277676b909aae7eae992ff166f
Valon Jashari <valonjashari18@gmail.com>
2021-03-09 11:39:23 +0100

    Basic_exercices(1 & 2)

---
+++ Basic/script.js
@@ -0,0 +1,37 @@
+var a = document.getElementById("firstname");
+var b = document.getElementById("lastname")
+var c = document.getElementById("age")
+
+if (a > 5){
+    a.style.color=a+"green";
+    //a.style.color = "green";
+}
+else {
+   a.style.color = "red";
+}
+document.getElementById("fname")
+
+//Exercise 2
+var d = document.getElementById("select");
+
+for(let i=0; i<d.length;i++)
+{
+    if(d[i] == "IT")
+    {
+        d.style.backgroundColor=+"purple";
+    }
+    else if(d[i] == "Hospitality")
+    {
+        d.style.backgroundColor=+"yellow";
+    }
+    else if(d[i] == "Managment")
+    {
+        d.style.backgroundColor=+"blue";
+    }
+    else
+    {
+        d.style.backgroundColor="gray";
+        //d.style.background=+"gray";
+    }
+}
+function profession(x){'click'}
